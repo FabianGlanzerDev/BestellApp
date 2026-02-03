de67403e122024b9dd16ffcab9086417afd1703f
Fabian Glanzer <Fabian.glanzer99@gmx.at>
2026-02-03 18:17:03 +0100

    responisve/fixes

--- script.js
+++ script.js
@@ -11,16 +11,6 @@ function formatPrice(number) {
     return number.toFixed(2).replace(".", ",") + " €";
 }
 
-// Warenkorb als Objekt speichern
-let basket = {};
-
-// Elemente vom Warenkorb holen
-let basketItems = document.querySelector(".basket-items");
-let subtotalText = document.querySelector(".basket-subtotal");
-let deliveryText = document.querySelector(".basket-delivery-cost");
-let totalText = document.querySelector(".basket-total-value");
-
-let deliverySwitch = document.querySelector(".basket-switch input");
 
 // Artikel zum Warenkorb hinzufügen
 function addToBasket(dish) {
